77de7d480333d9a91e1c12035dc4cbda67b25818
MemoryAndDream <312141830@qq.com>
2023-10-29 19:40:37 +0800

    update

--- test_code/test3.py
+++ test_code/test3.py
@@ -10,6 +10,7 @@ date：          2023/10/15
 import jieba
 import re
 import math
+import sys
 
 
 def read_file():
@@ -24,7 +25,7 @@ def f_tired(t):  # 不疲劳容易死循环
     return t / 10 if t < 10 and t > 0 else 1
 
 
-def f_strengthen(count):
+def f_strengthen(count):  # 与衰减冲突!!但是不加这个会导致助词权重过大 需要重新设计关系！
     return 1 - math.e ** -count
 
 
@@ -33,7 +34,7 @@ def f_link(linked):
 
 
 def f_forget(d):
-    return 1 / (2 ** d)
+    return 1 / (1.5 ** d)  # 2的话前面所有词相加都不如最近的词，1则是失去顺序性
 
 
 def new_learn():
@@ -71,33 +72,53 @@ def new_learn():
 
 
 def first_read(words_dicts, input):
-    last_word = input
     print(input)
     output_dicts = {}
-
+    previous_words = ['', '', '', '', '', input]
     for i in range(100):
-        # 不断输出下一个最强词。如果思维中断了，那就从最近几个词找。
+        # 不断输出下一个最强词。
+        last_word = previous_words[-1]
         if not words_dicts[last_word]:
             break
         else:
             max_exciting, next_word = 0, ''
-            for word in words_dicts[last_word]:
-                t, activate_count = words_dicts[last_word][word]
-                exciting = f_tired(i - t) * f_strengthen(activate_count) * f_tired(i - output_dicts.get(word, 0))
+            for word in words_dicts[last_word]:  # 这里简化计算，从和最近一个词有联系的词里算
+                exciting = 0
+                for d in range(1, 7, 1):
+                    old_word = previous_words[-d]
+                    if not old_word:
+                        continue
+
+                    t, activate_count = words_dicts[old_word].get(word, [0, 0])
+                    if activate_count:
+                        exciting += f_tired(i - t) * activate_count * f_tired(
+                            i - output_dicts.get(word, 0))  # 注意，记的时候已经计算过距离衰减了，回忆的时候就不用了
+
                 # 这里不对！回忆要和记忆的逻辑整合！不能只看上一个词 代码整合一下
                 if exciting > max_exciting:
                     max_exciting, next_word = exciting, word
-            words_dicts[last_word][next_word] = [i, words_dicts[last_word][next_word][1] + 1]
-            last_word = next_word
+            # words_dicts[last_word][next_word] = [i, words_dicts[last_word][next_word][1] + 1]  # 先不考虑疲劳和回忆中记忆s
             output_dicts[last_word] = i
+            previous_words.pop(0)
+            previous_words.append(next_word)
             print(next_word, end='')
 
 
 def main():
     words_dicts = new_learn()
     input = '北极'
+    # a = words_dicts['北极']
+    # b = words_dicts['光照']
     first_read(words_dicts, input)
+    # c = 3
+    # # 北极的 1.3 北极在 0.4
+    # 光照的
+    # 1.3
+    # 光照在
+    # 2.8
 
 
 if __name__ == '__main__':
     main()
+
+# 北极 光照 -的/在
