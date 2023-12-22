f2a94009ba9ec996bab1a6251e2b098878f5ce9e
unknown <312141830@qq.com>
2023-12-22 15:34:05 +0800

    mengmeng init

---
+++ mengmeng/life.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Description :
+date：          2023/12/22
+"""
+from she import MengMeng
+
+def live():
+    # born
+    meng_meng = MengMeng()
+    meng_meng.live()
+
+
+
+if __name__ == '__main__':
+    live()
---
+++ mengmeng/she.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Description :
+date：          2023/12/22
+"""
+from body import  *
+
+class MengMeng:
+    def __init__(self):
+        pass
+
+    def live(self):
+        # As time goes by, she lives
+        self.get_input()
+        self.do_something()
+        self.refresh_status()
+
+
+    def get_input(self):
+        pass
+
+
+    def do_something(self):
+        pass
+
+    def refresh_status(self):
+        pass
