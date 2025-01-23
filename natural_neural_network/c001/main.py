3d28ec955d1b7ce1f59a2a3c3e1642987f1b65d8
MemoryAndDream <312141830@qq.com>
2025-01-23 22:41:12 +0800

    update

--- natural_neural_network/c001/main.py
+++ natural_neural_network/c001/main.py
@@ -30,27 +30,42 @@ o(吃)o  o(逃跑)
 '''
 
 class Synapse:
-    def __init__(self, connected_neuron, init_weight=1):
-        self.connected_neuron = connected_neuron
+    def __init__(self, from_neuron, to_neuron, init_weight=1): # from to 是神经冲动传导方向 连接是从源头(高层)连接底层
+        self.from_neuron = from_neuron
+        self.to_neuron = to_neuron
         self.weight = init_weight
 
 class Neuron:
     def __init__(self, name=''):
-        self.synapses = []
+        self.after_synapses = []
+        self.forward_synapses = []
         self.name = name
 
-    def connect(self, neuron, weight=1):
-        synapse = Synapse(neuron, weight)
-        self.synapses.append(synapse)
+    def connect(self, neuron, weight=1): # after 是下游神经元 forward是上游 感觉有问题啊
+        synapse = Synapse(self, neuron, weight)
+        self.after_synapses.append(synapse)
+        neuron.forward_synapses.append(synapse)
 
     def activate(self):
-        # 神经元激活
-        pass
+        # 神经元激活 1 根据刺激强化下游树突 2 刺激下层神经元
+        for sy in self.after_synapses:
+            sy.weight = sy.weight*1.5
+            all_sys = sy.from_neuron.forward_synapses
+            for sy in all_sys:
+                sy.weight = sy.weight*0.9
+
+
 
     def stimulate(self):
         # 刺激神经元
         pass
 
+    def print_synapses(self):
+        # print('sy name',self.name)
+        for sy in self.forward_synapses:
+            print(sy.from_neuron.name,' to ', sy.to_neuron.name, sy.weight)
+        for sy in self.after_synapses:
+            print(sy.from_neuron.name,' to ', sy.to_neuron.name, sy.weight)
 
 # 从底层一步步往上定义 定义方式：底层为l1o1
 
@@ -59,16 +74,16 @@ def born():
     l1o2 = Neuron('see')
     l1o3 = Neuron('run')
 
-    l2o1 = Neuron()
+    l2o1 = Neuron('l2o1')
     l2o1.connect(l1o1,2)
     l2o1.connect(l1o2)
 
-    l2o2 = Neuron()
+    l2o2 = Neuron('l2o2')
     l2o2.connect(l1o1)
     l2o2.connect(l1o2,2)
     l2o2.connect(l1o3)
 
-    l2o3 = Neuron()
+    l2o3 = Neuron('l2o3')
     l2o3.connect(l1o2)
     l2o3.connect(l1o3,2)
 
@@ -86,8 +101,25 @@ def born():
     l3o3.connect(l2o2)
     l3o3.connect(l2o3,2)
 
+    # test1  食 眼 食 眼 痛
+    l3o1.activate()
+    l3o2.activate()
+    l3o1.activate()
+    l3o2.activate()
+    l3o1.activate()
+    l3o2.activate()
+    l3o1.activate()
+    l3o2.activate()
+    l3o3.activate()
+
+    l2o1.print_synapses()
+    l2o2.print_synapses()
+    l2o3.print_synapses()
+
+
 
 
 
 if __name__ == '__main__':
-    main()
+    born() # 1只有走完全流程才有共同作用部分，单层是没有用的
+    # 似乎吃和痛没有构成互斥，完全是在独立加强，的确符合自觉
