f83d3bb11a983af1462d2e9a1149c0ba8d6075e5
MemoryAndDream <312141830@qq.com>
2025-04-30 23:35:32 +0800

    update

--- natural_neural_network/m003/main.py
+++ natural_neural_network/m003/main.py
@@ -2,14 +2,22 @@
 # 本来是打算简谱的，但是搞简谱有点麻烦，所以用单词记忆好像也行， 比如 good apple
 # 学习过程为：看一个'apple'，以及apple这个单词一个个字母输入
 # 期望结果：按顺序输出apple
+
+
+# 由于是带抽象的，所以不能按阈值激活，而是只能作比较器
+
 class Node:
-    def __init__(self,name): # id 必须为唯一
+    def __init__(self,name,depth=0): # id 必须为唯一
         self.id=id(self)#
         self.name=name
         # self.id = name
         self.active_level = 0
         self.link_dict = {}
+        self.depth = depth
 
+
+    def is_blank_node(self):
+        return False if self.link_dict else True
 
     def init_connect(self, next_node, strength=100):
         link = Link(self.id,next_node.id, strength)
@@ -19,6 +27,13 @@ class Node:
     def act(self): # 激活下级node，将本次链接强度加入
         for next_node, link in self.link_dict.items():
             next_node.active_level += link.strength
+
+    def has_empty_sub_node(self):
+        for sub_node in self.link_dict:
+            if sub_node.is_blank_node():
+                return sub_node
+        return None
+
 
 
 
@@ -54,7 +69,7 @@ class Mind:
         return node
 
     def init_sub_nodes(self, node: Node):
-        # generate level 2,3 node for the first level node
+        # 创建初始化的2层节点
         level_1_node_name = node.name + '_1'
         level_2_node_name = node.name + '_2'
         node_1 = self.add_node(level_1_node_name)
@@ -62,10 +77,16 @@ class Mind:
         self.init_connect(node, node_1)
         self.init_connect(node_1, node_2)
 
-    def activate_node(self, init_node_id):
+    def activate_node(self, init_node: Node):
         # init node被视为接收输入的node,所以处理事件只需要激活这一类node即可
-        init_node = self.all_nodes[init_node_id]
         init_node.act() # 这里根据链接激活下游节点
+
+        # 产生新的空白节点和基于附近时间激活的新链接
+        # 需要迭代计算
+        if not init_node.has_empty_sub_node(): # 需要注意不要无限拉长空白节点链的长度
+            new_empty_node = self.add_node()
+            init_node.init_connect(new_empty_node)
+
 
 
     def create_or_strenth_link(self, source, target, strength=1):
@@ -89,7 +110,7 @@ class Mind:
         # 计算这一时间发生的神经元活动
         # 先简化为当前只有一个target被激活
         for link in self.all_links:
-            if link.active_at > self.time_stamp -3:
+            if link.active_at > self.time_stamp - 3:
                 self.create_or_strenth_link(link.source_id,target_id) # 这里其实要优化，越近的加强越大
         self.time_stamp += 1
 
@@ -110,12 +131,14 @@ def progress(baby_mind:Mind):
     # 训练事件 看见apple，然后依次输入 a p p l e
 
     for retry in range(10):
-        apple.act()
-        a.act()
-        p.act()
-        p.act()
-        l.act()
-        e.act()
+        baby_mind.activate_node(apple)
+        baby_mind.activate_node(a)
+        baby_mind.activate_node(p)
+        baby_mind.activate_node(p)
+        baby_mind.activate_node(l)
+        baby_mind.activate_node(e)
+
+
 
 def detect_links(baby_mind:Mind):
     for link in baby_mind.all_links:
